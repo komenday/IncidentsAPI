03eb76cdc379a224c8792ecfc2286cc819510c4d
Yurii Komenda <komenday2gmail.com>
2022-11-07 21:55:07 +0200

    Edited seed data

--- bART_TestProject/Models/IncidentsContext.cs
+++ bART_TestProject/Models/IncidentsContext.cs
@@ -20,15 +20,15 @@ namespace bART_TestProject.Models
                 .HasIndex(acc => acc.Name)
                 .IsUnique();
 
-            Account me = new Account { Id = 1, Name = "me" };
+            Account colleagues = new Account { Id = 1, Name = "colleagues" };
             Account friends = new Account { Id = 2, Name = "friends" };
 
             Contact yurii = new Contact { Id = 1, FirstName = "Yurii", LastName = "Komenda", Email = "komenday@gmail.com", AccountId = 1 };
-            Contact nazar = new Contact { Id = 2, FirstName = "Nazar", LastName = "Datskiv", Email = "datckivn@gmail.com", AccountId = 2 };
-            Contact max = new Contact { Id = 3, FirstName = "Max", LastName = "Slivinskiy", Email = "slvnskmx@gmail.com", AccountId = 2 };
+            Contact john = new Contact { Id = 2, FirstName = "John", LastName = "Doe", Email = "john@gmail.com", AccountId = 2 };
+            Contact richard = new Contact { Id = 3, FirstName = "Richard", LastName = "Roe", Email = "rich@gmail.com", AccountId = 2 };
 
-            builder.Entity<Contact>().HasData(yurii, nazar, max);
-            builder.Entity<Account>().HasData(me, friends);
+            builder.Entity<Contact>().HasData(yurii, john, richard);
+            builder.Entity<Account>().HasData(colleagues, friends);
 
             base.OnModelCreating(builder);
         }
