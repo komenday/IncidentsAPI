6716a20b0d32e950a991fe6e3c12e1473731900f
Yurii Komenda <komenday2gmail.com>
2022-11-07 21:42:01 +0200

    Incident is added to project

---
+++ bART_TestProject/Models/Incident.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bART_TestProject.Models
+{
+    public class Incident
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
