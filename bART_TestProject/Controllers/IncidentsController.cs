ac7b55a714e40c4fd98ba22a0312152ca9f150b2
Yurii Komenda <komenday2gmail.com>
2022-11-07 21:40:11 +0200

    Test project for bART is done

---
+++ bART_TestProject/Controllers/IncidentsController.cs
@@ -0,0 +1,55 @@
+﻿using bART_TestProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace bART_TestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentsController : ControllerBase
+    {
+        private readonly IncidentsContext _context;
+        public IncidentsController(IncidentsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Incidents!.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] RequestEncapsulator request)
+        {
+            if (!_context.Accounts!.Any(acc => acc.Name == request.AccountName))
+                return NotFound();
+
+            Account account = await _context.Accounts!.FirstAsync(acc => acc.Name == request.AccountName);
+            Contact? contact = await _context.Contacts!.FirstOrDefaultAsync(cont => cont.Email == request.ContactEmail);
+            if (contact is not null)
+            {
+                await new ContactsController(_context).Put(request);
+                return Ok();
+            }
+            else
+            {
+                Incident incident = new Incident
+                {
+                    Description = request.IncidentDescription,
+                    Accounts = new List<Account>()
+                };
+                incident.Accounts.Add(account);
+
+                _context.Incidents!.Add(incident);
+                await _context.SaveChangesAsync();
+
+                await new ContactsController(_context).Post(request);
+                return Ok();
+            }
+        }
+    }
+}
---
+++ bART_TestProject/Models/IncidentsContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace bART_TestProject.Models
+{
+    public class IncidentsContext : DbContext
+    {
+        public DbSet<Incident>? Incidents { get; set; }
+        public DbSet<Account>? Accounts { get; set; }
+        public DbSet<Contact>? Contacts { get; set; }
+
+        public IncidentsContext(DbContextOptions<IncidentsContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Account>()
+                .HasIndex(acc => acc.Name)
+                .IsUnique();
+
+            Account me = new Account { Id = 1, Name = "me" };
+            Account friends = new Account { Id = 2, Name = "friends" };
+
+            Contact yurii = new Contact { Id = 1, FirstName = "Yurii", LastName = "Komenda", Email = "komenday@gmail.com", AccountId = 1 };
+            Contact nazar = new Contact { Id = 2, FirstName = "Nazar", LastName = "Datskiv", Email = "datckivn@gmail.com", AccountId = 2 };
+            Contact max = new Contact { Id = 3, FirstName = "Max", LastName = "Slivinskiy", Email = "slvnskmx@gmail.com", AccountId = 2 };
+
+            builder.Entity<Contact>().HasData(yurii, nazar, max);
+            builder.Entity<Account>().HasData(me, friends);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
